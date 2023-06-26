30afa3173ee4512b7c659107c8cc30a0f4958683
Abdohm-coder <hadjmedabdo@gmail.com>
2023-06-26 11:20:41 +0100

    create/delete comments

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -14,6 +14,21 @@ module.exports = gql`
     body: String!
     username: String!
     createdAt: String!
+    comments: [Comment]!
+    likes: [Like]!
+  }
+
+  type Comment {
+    id: ID!
+    createdAt: String!
+    username: String!
+    body: String!
+  }
+
+  type Like {
+    id: ID!
+    createdAt: String!
+    username: String!
   }
 
   input RegisterInput {
@@ -33,5 +48,8 @@ module.exports = gql`
     login(username: String!, password: String!): User!
     createPost(body: String!): Post!
     deletePost(postId: ID!): String!
+    createComment(postId: ID!, body: String!): Post!
+    deleteComment(postId: ID!, commentId: ID!): Post!
+    likePost(postId: ID!): Post!
   }
 `;
